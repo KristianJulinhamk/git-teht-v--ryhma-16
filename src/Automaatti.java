707dbfd81ceabab768b2a1fe2d30b32669cef31d
Timo Peltokangas <timo.peltokangas@student.hamk.fi>
2024-12-18 20:15:04 +0200

    Versio2

--- src/Automaatti.java
+++ src/Automaatti.java
@@ -2,50 +2,47 @@ import java.util.Scanner;
 
 public class Automaatti {
     public static void main(String args[] ) {
-        //tilin saldo :(
-        int balance = 500, withdraw;
+
+        // Pankkiautomaatti ohjelma RYHMA 16
+
+        int balance = 500, withdraw;            // Muuttujat balance(Tilin saldo) ja withdraw
 
         Scanner sc = new Scanner(System.in);
-          
+
         while(true) {
             System.out.println("Pankkiautomaatti");
             System.out.println("Paina 1 nostaaksesi rahaa");
             System.out.println("Paina 2 tarkistaaksesi tilisi saldo");
             System.out.println("Paina 3 lopettaaksesi");
-              
-            //ottaa vaihtoehdon minkä käyttäjä laittaa
-            int vaihtoehto = sc.nextInt();
 
-            switch(vaihtoehto) {
-            case 1:
+            int vaihtoehto = sc.nextInt();      // Tallennetaan kayttajan syote muuttujaan vaihtoehto
+
+            switch(vaihtoehto) {                // Switch valitaan vaihtoehto 1,2,3
+
+            case 1:                             // CASE 1. Kysytaan kayttajan haluama rahamaara.
             System.out.print("Kuinka paljon haluat nostaa rahaa?");
-                      
-            //ottaa noston
-            withdraw = sc.nextInt();
-                      
-            //katotaan että riittääkö fyrkka
-            if(balance >= withdraw) {  
-            //poistaa nostomäärän saldosta 
-            balance = balance - withdraw;  
+            withdraw = sc.nextInt();            // Luetaan syote ja tallennetaan muuttujaan withdraw
+
+            if(balance >= withdraw) {           // Tarkistetaan riittaako rahat. 
+            balance = balance - withdraw;       // Vahennetaan nostettu rahamaara saldosta.
             System.out.println("Ottakaa rahanne");
             } 
-            else {  
-            //error viesti 
+            else {                              // Mikali rahat ei riita.
             System.out.println("Ei riitä rahat :(");
             }  
             System.out.println("");  
             break;
 
-            case 2:
-            //nayttaa tilin saldon
+
+            case 2:                             // CASE 2. Tulostetaan tilin saldo.
             System.out.println("Tilin saldo: "+balance);
             System.out.println("");
             break;
-   
-            case 3:
-            //exit
-            System.exit(0); }
-              
+
+
+            case 3:                             // CASE 3. Lopetus. EXIT
+            System.exit(0); 
+            }
         }  
     }  
 }
